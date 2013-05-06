519504218e3d680a0dead4d0df000eac3d089391
Matthew Martz <xiao_haozi@mutaku.com>
2013-05-05 22:57:41 -0400

    cap killer

--- slapbits.py
+++ slapbits.py
@@ -194,5 +194,5 @@ if __name__ == '__main__':
         if opt == '-p':
             vars['port'] = int(arg)
 
-    Print "Running with {}".format(vars)
+    print "Running with {}".format(vars)
     app.run(**vars)
